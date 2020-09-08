605926150fa63802a6b1eb5fa55b83c020a8d072
Horacio <horacio.crotte@gmail.com>
2020-09-08 12:14:40 -0500

    SCL7

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-export SCRAM_ARCH=slc6_amd64_gcc700
+export SCRAM_ARCH=slc7_amd64_gcc700
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-if [ -r CMSSW_10_2_20/src ] ; then
- echo release CMSSW_10_2_20 already exists
+if [ -r CMSSW_10_2_20_UL/src ] ; then
+ echo release CMSSW_10_2_20_UL already exists
 else
-scram p CMSSW CMSSW_10_2_20
+scram p CMSSW CMSSW_10_2_20_UL
 fi
-cd CMSSW_10_2_20/src
+cd CMSSW_10_2_20_UL/src
 eval `scram runtime -sh`
 
 pyfile="fragment.py"
